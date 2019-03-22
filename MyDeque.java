0367473487a069a94c8dd22c9ff48dd7a0f077c2
Kayla Fang <kfang00@stuy.edu>
2019-03-22 10:52:06 -0400

    constructors and size

--- MyDeque.java
+++ MyDeque.java
@@ -2,14 +2,49 @@ public class MyDeque<E>{
   private E[] data;
   private int size, start, end;
 
-  public MyDeque(){  }
-  public MyDeque(int initialCapacity){  }
-  public int size(){ }
-  public String toString(){ }
-  public void addFirst(E element){ }
-  public void addLast(E element){ }
-  public E removeFirst(){ }
-  public E removeLast(){ }
-  public E getFirst(){ }
-  public E getLast(){ }
+  public MyDeque(){
+    E[] data = new E[];
+    int size = 0;
+    int start = 0;
+    int end = 0;
+  }
+  public MyDeque(int initialCapacity){
+    E[] data = new E[initialCapacity];
+    int size = initialCapacity;
+    int start = 0;
+    int end = 0;
+  }
+
+  public int size(){
+    return size;
+  }
+
+  public String toString(){
+
+  }
+
+  public void addFirst(E element){
+
+  }
+
+  public void addLast(E element){
+
+  }
+
+  public E removeFirst(){
+
+  }
+
+  public E removeLast(){
+
+  }
+
+  public E getFirst(){
+
+  }
+
+  public E getLast(){
+
+  }
+
 }
