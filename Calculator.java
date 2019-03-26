1897ca7de9fdf5d5dc830563cd4f8fa8b8a2e2df
Kayla Fang <kfang00@stuy.edu>
2019-03-26 04:52:20 -0400

    starting to split for eval

---
+++ Calculator.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Calculator{
+    /*Evaluate a postfix expression stored in s.
+     *Assume valid postfix notation, separated by spaces.
+     */
+    public static double eval(String s){
+        MyDeque<E> hold = new MyDeque<E>()
+	String[] str = new String[s.length()];
+        str = s.split(" ");
+        System.out.println(str);
+        return 0.0;
+    }
+}
