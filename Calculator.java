04db30560ef418eaa3bd8bdb11f257c32c833931
Kayla Fang <kfang00@stuy.edu>
2019-03-26 10:52:50 -0400

    did + - *

--- Calculator.java
+++ Calculator.java
@@ -1,25 +1,42 @@
-<<<<<<< HEAD
-=======
 import java.util.*;
 import java.io.*;
 
->>>>>>> 1897ca7de9fdf5d5dc830563cd4f8fa8b8a2e2df
 public class Calculator{
     /*Evaluate a postfix expression stored in s.
      *Assume valid postfix notation, separated by spaces.
      */
     public static double eval(String s){
-<<<<<<< HEAD
-      int hold = 0;
+      String hold = 0;
+      Double hold2 = 0;
+      MyDeque<Double> de = new MyDeque<>();
+	    Scanner in = new Scanner(s);
+      while (in.hasNext()) {
+        hold = in.next();
+        if (hold = "+") {
+          hold2 = de.removeLast() + de.removeLast();
+          de.addLast(hold2);
+        }
+        if (hold = "-") {
+          hold2 = de.removeLast();
+          hold2 = de.removeLast() - hold2;
+          de.addLast(hold2);
+        }
+        if (hold = "*") {
+          hold2 = de.removeLast() * de.removeLast();
+          de.addLast(hold2);
+        }
+        if (hold = "/") {
+          hold2 = de.removeLast() * de.removeLast();
+          de.addLast(hold2);
+        }
+        if (hold = "%") {
 
+        }
+        else {
+          hold2 = Double.parseDouble(hold);
+          de.addLast(hold2);
+        }
+      }
+      return 0.0;
     }
 }
-=======
-        MyDeque<E> hold = new MyDeque<E>()
-	String[] str = new String[s.length()];
-        str = s.split(" ");
-        System.out.println(str);
-        return 0.0;
-    }
-}
->>>>>>> 1897ca7de9fdf5d5dc830563cd4f8fa8b8a2e2df
